88c7b46261d678e33d6663610511696d3a67a5e4
MJLew <michael.lew241@gmail.com>
2019-08-15 17:46:31 -0400

    Create FoodItem.java

---
+++ WhatsForDinner/app/src/main/java/com/example/michael/whatsfordinner/FoodItem.java
@@ -0,0 +1,12 @@
+package com.example.michael.whatsfordinner;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class FoodItem {
+    String name;
+    HashMap<String, Boolean> filtersMap;
+    List<FoodItem> subItems;
+
+
+}
